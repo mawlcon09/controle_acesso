9f5ccbadaa15dbe449e172b2a1854f725838fd6f
mawlcon09 <mawlcondomeciano@gmail.com>
2026-02-11 16:58:15 -0300

    idade

--- backend/acesso.php
+++ backend/acesso.php
@@ -1,4 +1,16 @@
 <?php
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    $idade = $_POST["idade"];
 
+    if ($idade < 16) {
+
+        echo "Acesso proibido";
+
+    } elseif ( ($idade >= 16 && $idade <= 17)) {
+        echo "Acesso permitido com responsaveis";
+
+    }
+}
 ?>
